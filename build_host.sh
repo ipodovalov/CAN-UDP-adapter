2362cb0ff1add717b2e7801d5856afebf9596430
Ivan Podovalov <38607341+ipodovalov@users.noreply.github.com>
2019-05-20 21:59:44 +0300

    Для сборки под HOST-машину скрипт написал

--- build_host.sh
+++ build_host.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+cd libs/elogger/build-gcc/
+cmake ..
+make
+
+cd ../../libini/build-gcc/
+cmake ..
+make
+
+cd ../../apps/can_worker
+./build.sh HOST
+
+cd ../../apps/udp_worker
+./build.sh HOST
