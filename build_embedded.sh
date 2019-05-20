6948719accfc3330246f9d0b1eeb5753f2db3bfb
Ivan Podovalov <38607341+ipodovalov@users.noreply.github.com>
2019-05-20 21:57:11 +0300

    Общий скрипт для сборки под Embedded Linux написал

--- build_embedded.sh
+++ build_embedded.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+cd libs/elogger/build/
+cmake -DCMAKE_TOOLCHAIN_FILE=../../cmake/buildroot.cmake ..
+make
+
+cd ../../libini/build/
+cmake -DCMAKE_TOOLCHAIN_FILE=../../cmake/buildroot.cmake ..
+make
+
+cd ../../apps/can_worker
+./build.sh
+
+cd ../../apps/udp_worker
+./build.sh
